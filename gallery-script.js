e12f0f3d3a0834e1b5431d7e666aba1193434f72
jasper francisco <119076814+Peper8@users.noreply.github.com>
2023-01-23 18:59:42 +0800

    Add files via upload

---
+++ gallery-script.js
@@ -0,0 +1,43 @@
+/*PORTFOLIO GALLERY SCRIPT*/
+
+const filterContainer = document.querySelector(".gallery-filter"),
+galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) =>{
+  if(event.target.classList.contains("filter-item")){
+    //deactivate existing active 'filter-item'
+    filterContainer.querySelector(".active").classList.remove("active");
+    // activate new 'filter-item'
+    event.target.classList.add("active")
+    const filterValue = event.target.getAttribute("data-filter");
+    galleryItems.forEach((item) =>{
+      if(item.classList.contains(filterValue) || filterValue === 'all'){
+        item.classList.remove("hide");
+        item.classList.add("show");
+      }
+      else{
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+    });
+  }
+})
+
+
+/*ON CLICK PREVIEW SCRIPT*/
+
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+
+
+function openFullImg(pic){
+  fullImgBox.style.display = "flex";
+  fullImg.src = pic;
+}
+
+function closeFullImg(){
+  fullImgBox.style.display = "none";
+}
+
+
