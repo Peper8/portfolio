8581f7eac99579b6de6f7b9ebc726eb4ec080a7c
jasper francisco <119076814+Peper8@users.noreply.github.com>
2023-01-27 12:27:25 +0800

    Delete typing-effect.js

--- typing-effect.js
+++
@@ -1,8 +0,0 @@
-
-  var typed = new Typed(".input",{
-    strings:["Jasper Francisco.","a Front-end Developer.","a Web Designer.","a Graphic Designer","a Virtual Assistant.","a Gamer."],
-    typeSpeed: 70,
-    backSpeed: 60,
-    loop: true
-  });
-
